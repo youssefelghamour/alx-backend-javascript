69607bdfe2c8ae9dfccf5e4e0568b9decb40e698
youssef <youssefelghamour@gmail.com>
2024-06-04 17:49:09 +0000

    add 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
 
   const result = [];
 
   for (const element of set) {
-    if (startString && element.startsWith(startString)) {
+    if (typeof element === 'string' || startString && element.startsWith(startString)) {
       const newStr = element.slice(startString.length);
       if (newStr && newStr !== element) {
         result.push(newStr);
