e02b008a700fb6da8a320fa5919abca8beb46b66
youssef <youssefelghamour@gmail.com>
2024-06-04 14:46:46 +0000

    add 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const obj1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Fransisco',
+  };
+
+  const obj2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'San Fransisco',
+  };
+
+  const obj3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [obj1, obj2, obj3];
+}
