8409fb22aa469a81fffd25faeb1a61441c7a25aa
youssef <youssefelghamour@gmail.com>
2024-06-05 21:53:52 +0100

    #5 - task_2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,50 @@
+export interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+export interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+    workFromHome(): string {
+        return 'Working from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break'
+    }
+
+    workDirectorTasks(): string {
+        return 'Getting to director tasks'
+    }
+}
+
+export class Teacher implements TeacherInterface {
+    workFromHome() {
+      return 'Cannot work from home';
+    }
+  
+    getCoffeeBreak() {
+      return 'Cannot have a break';
+    }
+  
+    workTeacherTasks() {
+      return 'Getting to work';
+    }
+  }
+
+export function createEmployee(salary: number | string): (Director | Teacher) {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
