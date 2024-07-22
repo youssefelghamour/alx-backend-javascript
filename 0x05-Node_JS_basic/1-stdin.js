452f43c5cd0537d195ce30a942da590570ee6b0c
youssefelghamour <youssefelghamour@gmail.com>
2024-07-22 18:31:40 +0000

    correct 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  
+
   if (input) {
     process.stdout.write(`Your name is: ${input}`);
   }
