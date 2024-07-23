ac9c5ad2f8f409b79ab34f13d6dab215b723f859
youssefelghamour <youssefelghamour@gmail.com>
2024-07-23 09:37:51 +0000

    add 2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+
+const countStudents = (path) => {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+
+  const data = fs.readFileSync(path, 'utf8');
+  const lines = data.trim().split('\n'); // Split the data into lines
+  const students = lines.slice(1); // Remove the header line
+
+  const totalStudents = students.length; // Count the total number of students
+  console.log(`Number of students: ${totalStudents}`);
+
+  // Initialize an object to hold the count of students in each field
+  const fields = {};
+
+  students.forEach((line) => {
+    const parts = line.split(','); // Split each line by comma
+    const field = parts[3]; // The field is the fourth element in the array
+    const firstname = parts[0]; // The firstname is the first element in the array
+
+    // If the field doesn't exist in the fields object,
+    // initialize it as an empty array
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(firstname); // Add the firstname to the respective field
+  });
+  
+  // fields = {
+  //   CS: [ 'Johann', 'Arielle', 'Jonathan', 'Emmanuel', 'Guillaume', 'Katie' ],
+  //   SWE: [ 'Guillaume', 'Joseph', 'Paul', 'Tommy' ]
+  // }
+
+  for (const field in fields) {
+    const count = fields[field].length;
+    const names = fields[field].join(', ');
+    console.log(`Number of students in ${field}: ${count}. List: ${names}`);
+  }
+};
+
+module.exports = countStudents;
