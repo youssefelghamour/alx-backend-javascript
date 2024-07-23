228fb8051a748b634fbba4068e02ca00477d028c
youssefelghamour <youssefelghamour@gmail.com>
2024-07-23 10:27:49 +0000

    add 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const { createServer } = require('http');
+
+const hostname = 'localhost';
+const port = 1245;
+
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
